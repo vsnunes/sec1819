e0978bc6f51b5fd47b8d63d7d350c7f0562e2140
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2019-03-16 16:56:45 +0000

    Adds unit test package to server

--- HDSNotary-Server/src/main/java/pt/ulisboa/tecnico/meic/sec/HDSNotaryServer/NotaryService.java
+++ HDSNotary-Server/src/main/java/pt/ulisboa/tecnico/meic/sec/HDSNotaryServer/NotaryService.java
@@ -1,5 +1,6 @@
 package pt.ulisboa.tecnico.meic.sec.HDSNotaryServer;
 
+import javafx.util.Pair;
 import pt.ulisboa.tecnico.meic.sec.interfaces.NotaryInterface;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -16,6 +17,16 @@ public class NotaryService extends UnicastRemoteObject implements NotaryInterfac
 
     @Override
     public boolean intentionToSell(int userId, int goodId) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public Pair<Integer, Boolean> getStateOfGood(int goodId) throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public boolean transferGood(int sellerId, int buyerId, int goodId) throws RemoteException {
         return false;
     }
 
---
+++ HDSNotary-Server/src/test/java/pt/ulisboa/tecnico/meic/sec/HDSNotaryServer/NotaryServiceTest.java
@@ -0,0 +1,11 @@
+package pt.ulisboa.tecnico.meic.sec.HDSNotaryServer;
+
+import org.junit.Test;
+
+public class NotaryServiceTest {
+
+    @Test
+    public void intentionToSellBasicTest() {
+
+    }
+}
