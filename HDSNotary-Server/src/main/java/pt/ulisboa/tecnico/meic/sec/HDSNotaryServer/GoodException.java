4c87368b10f9b9620fd47c3b2b495914728cb3c7
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2019-03-19 14:56:39 +0000

    Adds Good class test and exception

---
+++ HDSNotary-Server/src/main/java/pt/ulisboa/tecnico/meic/sec/HDSNotaryServer/GoodException.java
@@ -0,0 +1,16 @@
+package pt.ulisboa.tecnico.meic.sec.HDSNotaryServer;
+
+/**
+ * Generic class for Good (Items) exceptions
+ */
+public class GoodException extends Exception {
+
+    GoodException() {
+        super();
+    }
+
+    GoodException(String message) {
+        super(message);
+    }
+
+}
