ea991a1e7b5fcc0ae4f5ca7f34e0c17e67795c8b
mbelem <mbelem@sysmail.me>
2019-03-20 12:34:39 +0000

    Update NotaryService.java

--- HDSNotary-Server/src/main/java/pt/ulisboa/tecnico/meic/sec/HDSNotaryServer/NotaryService.java
+++ HDSNotary-Server/src/main/java/pt/ulisboa/tecnico/meic/sec/HDSNotaryServer/NotaryService.java
@@ -1,6 +1,8 @@
 package pt.ulisboa.tecnico.meic.sec.HDSNotaryServer;
 
 import pt.ulisboa.tecnico.meic.sec.interfaces.NotaryInterface;
+
+import java.io.*;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
@@ -16,15 +18,18 @@ public class NotaryService extends UnicastRemoteObject implements NotaryInterfac
 
     public NotaryService() throws RemoteException {
         super();
-        users = new HashMap<>();
-        goods = new HashMap<>();
+        if(!doRead()){
+            users = new HashMap<>();
+            goods = new HashMap<>();
+        }
+
     }
 
     @Override
     public boolean intentionToSell(int userId, int goodId, boolean bool) throws RemoteException {
         Good good = goods.get(goodId);
         good.setForSell(bool);
-
+        doWrite();
         return good.isForSell();
     }
 
@@ -48,6 +53,57 @@ public class NotaryService extends UnicastRemoteObject implements NotaryInterfac
                         return true;
                 }
             }
+        }
+        doWrite();
+        return false;
+    }
+
+    /*
+    To be called when state changes
+     */
+    public void doWrite(){
+        try {
+            FileOutputStream f = new FileOutputStream(new File("myObjects.txt"));
+            ObjectOutputStream o = new ObjectOutputStream(f);
+
+            o.writeObject(users);
+            o.writeObject(goods);
+
+            o.close();
+            f.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error initializing stream");
+        }
+    }
+
+    /*
+    To be called when notary service starts
+     */
+    public boolean doRead() {
+        try {
+            //path to be defined
+            FileInputStream fi = new FileInputStream(new File("somefile.txt"));
+            ObjectInputStream oi = new ObjectInputStream(fi);
+
+            // Read objects
+            HashMap<Integer, User> users = (HashMap<Integer, User>) oi.readObject();
+            HashMap<Integer, Good> goods = (HashMap<Integer, Good>) oi.readObject();
+
+            oi.close();
+            fi.close();
+
+            return true;
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error initializing stream");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found");
+            e.printStackTrace();
         }
         return false;
     }
