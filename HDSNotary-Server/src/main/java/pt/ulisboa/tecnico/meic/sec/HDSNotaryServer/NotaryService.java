ad8326d46e793d16419255d208b50bc617b29645
mbelem <mbelem@sysmail.me>
2019-05-08 21:04:57 +0100

    adds volatile

--- HDSNotary-Server/src/main/java/pt/ulisboa/tecnico/meic/sec/HDSNotaryServer/NotaryService.java
+++ HDSNotary-Server/src/main/java/pt/ulisboa/tecnico/meic/sec/HDSNotaryServer/NotaryService.java
@@ -46,7 +46,7 @@ public class NotaryService extends UnicastRemoteObject implements NotaryInterfac
     private static String TRANSACTIONSTMP_FILE;
 
     /** list for echos of all clients */
-    private ArrayList<ClientEcho> clientEchos;
+    private volatile ArrayList<ClientEcho> clientEchos;
 
     /** Number of notaries */
     private final int NUMBER_OF_CLIENTS = 5;
