dd3751316078ca80fbb7ce387955b18e85f2880e
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2019-05-08 19:00:16 +0100

    Adds notary keys

--- HDSNotaryLib/src/main/resources/certs/convert_pvk_pkcs8.sh
+++ HDSNotaryLib/src/main/resources/certs/convert_pvk_pkcs8.sh
@@ -6,5 +6,6 @@ mkdir -p java_certs
 for i in {1..5}
 do
     openssl pkcs8 -in user"$i".key -topk8 -nocrypt -out java_certs/private_user"$i"_pkcs8.pem
+    openssl pkcs8 -in notary"$i".key -topk8 -nocrypt -out java_certs/private_notary"$i"_pkcs8.pem
 
 done
